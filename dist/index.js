bc92550ae03f2fe60ed450bd2e5a06f41efbd6d5
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2025-06-10 22:47:43 +0300

    Stop tracking dist directory, now in .gitignore

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const app = express();
-const PORT = 8080;
-app.use(express.static("."));
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
