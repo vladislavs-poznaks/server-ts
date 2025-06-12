3e4b72ee6903d900f365f00b0ffaefe4c9a54348
vladislavs-poznaks <vladislavs.poznaks@viasmsgroup.com>
2025-06-12 08:40:29 +0300

    added profanity filter

--- src/handlers/validation.ts
+++ src/handlers/validation.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express"
 import { jsonResponse } from "./json.js";
 
+const profane = ['kerfuffle', 'sharbert', 'fornax']
+
 
 export const chirp = (req: Request, res: Response) => {
   type parameters = {
@@ -11,7 +13,11 @@ export const chirp = (req: Request, res: Response) => {
 
   if (params.body.length > 140) {
     jsonResponse(res, {error: "Chirp is too long"}, 400)
-  } else {
-    jsonResponse(res, {valid: true})
+
+    return
   }
+
+  const cleanedBody = params.body.split(' ').map(word => profane.includes(word.toLocaleLowerCase()) ? '****' : word).join(' ')
+
+  jsonResponse(res, {cleanedBody})
 }
