db22e61f2cf1c97457aa97d4929d5bf966ba768a
vladislavs-poznaks <vladislavs.poznaks@viasmsgroup.com>
2025-06-14 08:45:11 +0300

    fixed drizzle config, generated migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "src/db/schema.ts",
-  out: "src/migrations",
+  out: "src/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: "postgres://postgres:@localhost:5432/chirpy?sslmode=disable",
